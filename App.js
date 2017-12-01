1bf5203371432490196297128152494a54c56bba
unknown <14520515@gm.uit.edu.vn>
2017-12-01 13:42:13 +0700

    ~add Axios tool to HTTP Request

--- App.js
+++ App.js
@@ -1,57 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import AlbumList from './src/components/AlbumList';
+import { StackNavigator } from 'react-navigation';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const RootNavigator = StackNavigator({
+    AlbumList: {
+        screen: AlbumList,
+    },
+    /*DetailScreen: {
+        screen: DetailScreen,
+    },*/
 });
+//Creat a component
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <RootNavigator />
+    </View>
+);
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+//render it to the screen
+AppRegistry.registerComponent('frapho', () => App);
+export default App; 
